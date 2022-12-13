5a9260c83f2c7a401f73a4361ef4fec6281c7c0e
Dmytro Batarin <dmytro@batarin.dev>
2022-12-13 19:16:17 +0100

    Configured ios app

--- App.tsx
+++ App.tsx
@@ -78,7 +78,7 @@ const App = () => {
           style={{
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}>
-          <Section title="Step One">
+          <Section title="Step One 231232">
             Edit <Text style={styles.highlight}>App.tsx</Text> to change this
             screen and then come back to see your edits.
           </Section>
