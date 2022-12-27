fbef0d81fea507bfda6d4af771089c066838e1c6
Dmytro Batarin <dmytro@batarin.dev>
2022-12-27 20:31:30 +0100

    Add absolute path

---
+++ src/HomeScreen.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from 'react';
+import { View, Text } from 'react-native';
+
+interface HomeScreenProps {}
+
+export const HomeScreen: FC<HomeScreenProps> = ({}) => {
+  return (
+    <View>
+      <Text>123</Text>
+    </View>
+  );
+};
