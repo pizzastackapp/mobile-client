5a0cfd4057621f3b1b6bb8f77f43d6546a3609b4
Dmytro Batarin <dmytro@batarin.dev>
2023-02-21 21:13:20 +0100

    Add error notifcation

--- App.tsx
+++ App.tsx
@@ -4,6 +4,7 @@ import { HomeScreen } from '@app/screens/home/home.screen';
 import React, { useEffect } from 'react';
 import { SafeAreaView, View, StyleSheet, StatusBar } from 'react-native';
 import SplashScreen from 'react-native-splash-screen';
+import Toast from 'react-native-toast-message';
 
 const appScreenStyles = StyleSheet.create({
   container: {
@@ -25,6 +26,7 @@ const App = () => {
           <HomeScreen />
         </SafeAreaView>
       </View>
+      <Toast />
     </ApolloProvider>
   );
 };
